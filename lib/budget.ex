9ad139549c517f9d4ba4eaa2b0e3a43244a40904
Unknown <christopher.nyberg@gmail.com>
2017-03-09 17:31:42 -0800

    style change
    More syntactically close to Elixir code standards using pipes.

--- lib/budget.ex
+++ lib/budget.ex
@@ -18,14 +18,30 @@ defmodule Budget do
     File.read!("lib/transactions.csv")
       |> parse
       |> filter
+      |> normalize
   end
 
   defp parse(string) do
-    CSV.parse_string(string)
+    string
+    |> String.replace("\r", "")
+    |> CSV.parse_string
   end
 
   defp filter(rows) do
     Enum.map(rows, &Enum.drop(&1, 1))
+  end
+
+  defp normalize(rows) do
+    Enum.map(rows, &parse_amount(&1))
+  end
+
+  defp parse_amount([date, description, amount]) do
+    [date, description, parse_to_float(amount)]
+  end
+
+  defp parse_to_float(string) do
+    string
+    |> String.to_float
   end
 
 end
