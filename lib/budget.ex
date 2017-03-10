6857da619d18f1f00b791ed7d4ad8b377b53b808
Unknown <christopher.nyberg@gmail.com>
2017-03-09 17:41:59 -0800

    Pretty printed
    Very nice.

--- lib/budget.ex
+++ lib/budget.ex
@@ -19,6 +19,8 @@ defmodule Budget do
       |> parse
       |> filter
       |> normalize
+      |> sort
+      |> print
   end
 
   defp parse(string) do
@@ -42,6 +44,24 @@ defmodule Budget do
   defp parse_to_float(string) do
     string
     |> String.to_float
+    |> abs
+  end
+
+  defp sort(rows) do
+    Enum.sort(rows, &sort_asc_by_amount(&1, &2))
+  end
+
+  defp sort_asc_by_amount([_,_,prev],[_,_,next]) do
+    prev < next
+  end
+
+  defp print(rows) do
+    IO.puts "\n Transactions:"
+    Enum.each(rows, &print_to_console(&1))
+  end
+
+  defp print_to_console([date, description, amount]) do
+    IO.puts "#{date} #{description} \t$#{:erlang.float_to_binary(amount, decimals: 2)}"
   end
 
 end
--- lib/mix/tasks/list_transactions.ex
+++ lib/mix/tasks/list_transactions.ex
@@ -3,6 +3,6 @@ defmodule Mix.Tasks.ListTransactions do
 
     @shortdoc "List transactions from a CSV file."
     def run(_) do
-        Budget.list_transactions |> IO.inspect
+        Budget.list_transactions
     end
 end
